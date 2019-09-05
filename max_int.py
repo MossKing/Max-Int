1b33b95d121b46147811bc5542b9af91e38e289d
MossKing <simon.bau@hotmail.com>
2019-09-05 09:27:38 +0000

    Implementation complete

--- max_int.py
+++ max_int.py
@@ -1,2 +1,12 @@
 #Búa þarf til int input breytu og breytu til að halda utan um stærstu töluna sem hefur komið.
-#Síðan nota While loop til að keyra forritið þangað til að tala < 0 kemur fyrir og prenntar síðan max_int út
+#Nota While loop til að keyra forritið þangað til að tala < 0 kemur fyrir og prenntar síðan max_int út
+#Nota if/elif/else til að max_int taki hæsta gildið
+
+num = int(input("Enter a number: "))
+max_int = 0
+
+while num > 0:
+    if max_int < num:
+        max_int = num
+    num = int(input("Enter a number: "))
+print(max_int)
